e8c7a06fb38750d7d6d7c245755994e22638002c
ethan fregien <ethanfregien@gmail.com>
2022-01-13 19:32:52 -0600

    more service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,3 +7,31 @@ const cacheTime = [
     "/idb.js",
     "/style.css"
 ];
+
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(cacheName).then(cache => {
+        console.log("Files were pre cached");
+        return cache.addAll(cacheTime);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== cacheName && key !== cacheData) {
+              console.log("Removing cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
