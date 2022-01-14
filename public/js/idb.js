91f03d6378ef2351bf319e0389bb5cb16d27f7dc
ethan fregien <ethanfregien@gmail.com>
2022-01-14 13:47:35 -0600

    testing

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,18 +1,19 @@
 
-const indexedDB = window.indexedDB || window.mozIndexedDB || window.msIndexedDB;
+const indexedDB = window.indexedDB || window.mozIndexedDB || window.msIndexedDB || webkitIndexedDB;
+
 let db;
 const request = indexedDB.open("budget_db", 1);
 request.onsuccess = ({target}) => {
     db = target.result;
     console.log(db);
     if (navigator.onLine) {
-        checkDatabase ();
+        checkDatabase();
     }
 };
 
 function newRecord(record) {
-  const transaction = db.transaction(["loading"], "readwrite");
-  const store = transaction.objectStore("loading");
+  const transaction = db.transaction(["new_budget"], "readwrite");
+  const store = transaction.objectStore("new_budget");
 
   store.add(record);
 }
@@ -32,11 +33,11 @@ request.onsuccess = function (event) {
 };
 request.onerror = function (event) {
     console.log("Uh oh " + event.target.errorCode);
-  };
+};
   
-  function checkDB() {
-    const transaction = db.transaction(["loading"], "readwrite");
-    const store = transaction.objectStore("loading");
+function checkDB() {
+    const transaction = db.transaction(["new_budget"], "readwrite");
+    const store = transaction.objectStore("new_budget");
     const getAll = store.getAll();
   
     getAll.onsuccess = function () {
@@ -51,17 +52,18 @@ request.onerror = function (event) {
         })
           .then(response => response.json())
           .then(() => {
-            const transaction = db.transaction(["loading"], "readwrite");
-            const store = transaction.objectStore("loading");
+            const transaction = db.transaction(["new_budget"], "readwrite");
+            const store = transaction.objectStore("new_budget");
             store.clear();
           });
       }
     };
-  }
-  function deleteLoading() {
-    const transaction = db.transaction(["loading"], "readwrite");
-    const store = transaction.objectStore("loading");
+}
+
+function deleteLoading() {
+    const transaction = db.transaction(["new_budget"], "readwrite");
+    const store = transaction.objectStore("new_budget");
     store.clear();
-  }
+}
   
-  window.addEventListener("online", checkDB); 
+window.addEventListener("online", checkDB); 
